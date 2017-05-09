d96872fc313babd8713475c07e7c04ebaf22126f
Sonja Duric <duric.sonja@gmail.com>
2017-05-09 12:21:24 -0400

    second drill complete with bonus credit

--- drill1.js
+++ drill1.js
@@ -14,3 +14,32 @@ function hello (){
 function goodbye (){
 	console.log('Goodbye World!');
 }
+
+//DRILL 2
+
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// This is a "predicate function" - it's a function that only returns a boolean
+//const filteredNames = filter(myNames, function(name) {
+//    return name[0] === 'R';
+//});
+
+const filteredNames = filter(myNames, ((name) => name[0] === 'R')); 
+console.log(filteredNames); 
+//filter(myNames, function(name){}) => console.log(filteredNames) // => ['Rich', 'Ray']
+
+// TASK -- DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr, fn) {
+	retArray = [];
+	for (let i=0; i <arr.length; i++){
+		if(fn(arr[i])){
+			retArray.push(arr[i]);
+		}
+	}
+	return retArray;
+
+}
+
+
+
