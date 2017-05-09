ede9df1e0b7ec0132ed2718994f1bba2ee8e4e0f
williamtwobit <william@twobit.cc>
2017-05-09 15:33:14 -0400

    final commit

--- average.js
+++ average.js
@@ -1,17 +1,30 @@
 // noprotect
 // ^^ `noprotect` is here to prevent a bug with jsbin and for loops.
 
-function average(numbers) {
-  var sum = 0;
-  for (var i=0; i <= numbers.length-1; i++){
-    sum = sum + numbers[i];
-  }
-  var result = sum / numbers.length;
-  return result;
+// function average(numbers) {
+//   var sum = 0;
+//   for (var i=0; i <= numbers.length-1; i++){
+//     sum = sum + numbers[i];
+//   }
+//   var result = sum / numbers.length;
+//   return result;
+// }
+
+// function average(numbers){
+//   let sum = 0;
+//   let i = 0;
+//   while (i < numbers.length){
+//     sum += numbers[i];
+//     i++;
+//   }
+//   return sum/numbers.length;
+// }
+
+function average(numbers){
+  let sum = 0;
+  numbers.forEach(n => sum += n);
+  return sum / numbers.length;
 }
-
-
-
 
 /* From here down, you are not expected to 
    understand.... for now :)  
@@ -31,3 +44,23 @@ function testFunctionWorks(fn, input, expected) {
   }
   else {
     console.log(
+       'FAILURE: `' + fn.name + '([' + input + '])` should be ' + expected +
+      ' but was ' + fn(input)
+    );
+    return false;
+  }
+}
+
+(function runTests() {
+  var numList1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  var correctAns1 = 5.5;
+  var numList2 = [0, -1, 1];
+  var correctAns2 = 0;
+  
+  var testResults = [
+    testFunctionWorks(average, numList1, correctAns1),
+    testFunctionWorks(average, numList2, correctAns2)
+  ];
+  var numPassing = testResults.filter(function(result){ return result; }).length;
+  console.log(numPassing + ' out of ' + testResults.length + ' tests passing.')
+})();
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,29 +8,54 @@ function fizzBuzz(countTo)
   var i = 1;
   while (i <= countTo)
   {
-    //console.log(array);
-    if (i % 15 === 0)
-    {
-      array.push("fizzbuzz");
+    console.log(array);
+    // if (i % 15 === 0)
+    // {
+    //   array.push("fizzbuzz");
+    // }
+    // else if (i % 3 === 0)
+    // {
+    //   array.push("fizz");
+    // }
+    // else if(i % 5 === 0)
+    // {
+    //   array.push("buzz");
+    // }
+    // else
+    // {
+    //   array.push(i);
+    // }
+    switch(i % 15){
+      case 0:
+        array.push("fizzbuzz");
+        break;
+      case 3: case 6: case 9: case 12:
+        array.push("fizz");
+        break;
+      case 5: case 10:
+        array.push("buzz");
+        break;
+      default:
+        array.push(i);
     }
-    else if (i % 3 === 0)
-    {
-      array.push("fizz");
-    }
-    else if(i % 5 === 0)
-    {
-      array.push("buzz");
-    }
-    else
-    {
-      array.push(i);
-    }
+
+
+    // 5 % 5 = 0
+    // 10 % 5 = 0
+
+    // 5 % 15 = 5
+    // 10  % 15 = 10
+
+    // 3 % 15 = 3
+    // 6 % 15 = 6
+    // 30 % 15 = 0
+
+    // 0 - 14
     i++;
   }
+  console.log(array);
   return array;
 }
-
-
 
 
 
